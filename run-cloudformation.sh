75395eb075050266569218bd855f62cc59bc5f50
Bhanuchandar Chebrolu <bhanuchebrolu@gmail.com>
2017-02-26 17:52:44 -0800

    added cloudformation for ec2 instance

---
+++ run-cloudformation.sh
@@ -0,0 +1,1 @@
+aws cloudformation create-stack --stack-name vpn2 --template-url https://s3-us-west-2.amazonaws.com/my-bucket-bhanu-1234/ec2-cloudformation-eip.json --parameters ParameterKey=KeyName,ParameterValue=myaws-key-1234567 ParameterKey=Subnet,ParameterValue=subnet-5677ab1f
