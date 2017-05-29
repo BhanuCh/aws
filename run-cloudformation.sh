95060c299d5f70bdfdba68bab563653e6134b030
Bhanuchandar Chebrolu <bhanuchebrolu@gmail.com>
2017-05-28 22:30:30 -0700

    changed script

--- run-cloudformation.sh
+++ run-cloudformation.sh
@@ -1,1 +1,2 @@
+#comment added
 aws cloudformation create-stack --stack-name vpn2 --template-url https://s3-us-west-2.amazonaws.com/my-bucket-bhanu-1234/ec2-cloudformation-eip.json --parameters ParameterKey=KeyName,ParameterValue=myaws-key-1234567 ParameterKey=Subnet,ParameterValue=subnet-5677ab1f
